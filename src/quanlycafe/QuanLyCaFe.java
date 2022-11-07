8943e2facd1861f6fd4a237e396693ed8ee34a21
TrieuBao <baotrieu27122003@gmail.com>
2022-11-07 14:22:26 +0700

    DuAn1

---
+++ src/quanlycafe/QuanLyCaFe.java
@@ -0,0 +1,18 @@
+
+package quanlycafe;
+/*
+* Author : Triệu Phan Thiên Bảo - PS22325
+*/
+
+
+import java.util.*;
+import java.io.*;
+
+public class QuanLyCaFe {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
