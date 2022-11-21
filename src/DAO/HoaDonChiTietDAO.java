96e33eb6f3d7e2c31d1ea4b9c829b01c62a696c7
AkilaKun <100337336+AkilaKun@users.noreply.github.com>
2022-11-21 14:00:45 +0700

    Sang

--- src/DAO/HoaDonChiTietDAO.java
+++ src/DAO/HoaDonChiTietDAO.java
@@ -1,47 +1,85 @@
-
 package DAO;
 
 
 /*
 * Author : Triệu Phan Thiên Bảo - PS22325
-*/
+ */
 import EntityClass.HoaDonChiTiet;
+import Utils.JdbcHelper;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+public class HoaDonChiTietDAO extends QuanLiCFDAO<HoaDonChiTiet, Integer> {
 
-public class HoaDonChiTietDAO extends QuanLiCFDAO<HoaDonChiTiet, Integer>{
+    String Insert_SQL = "insert into HOADONCHITIET (MaHDCT, MaHD, MaMon, Soluong, ThanhTien, GhiChu) values (?,?,?,?,?,?)";
+    String Update_SQL = "update HOADONCHITIET set MaHD = ?, MaMon = ?, Soluong = ?, ThanhTien = ?, GhiChu = ? where MaHDCT = ? ";
+    String Delete_SQL = "delete from HOADONCHITIET where MaHDCT = ?";
+    String SelectAll_SQL = "select * from HOADONCHITIET";
+    String SelectByID_SQL = "select * from HOADONCHITIET where MaHDCT = ?";
 
     @Override
     public void insert(HoaDonChiTiet entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Insert_SQL,
+                entity.getMaHDCT(),
+                entity.getMaHD(),
+                entity.getMaMon(),
+                entity.getSoLuong(),
+                entity.getThanhTien(),
+                entity.getGhiChu());
     }
 
     @Override
     public void update(HoaDonChiTiet entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Update_SQL,
+                entity.getMaHDCT(),
+                entity.getMaHD(),
+                entity.getMaMon(),
+                entity.getSoLuong(),
+                entity.getThanhTien(),
+                entity.getGhiChu());
     }
 
     @Override
     public void delete(Integer key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Delete_SQL, key);
     }
 
     @Override
     public List<HoaDonChiTiet> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return selectBySQL(SelectAll_SQL);
     }
 
     @Override
     public HoaDonChiTiet selectById(Integer key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<HoaDonChiTiet> list = selectBySQL(SelectByID_SQL, key);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
 
     @Override
     protected List<HoaDonChiTiet> selectBySQL(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<HoaDonChiTiet> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                HoaDonChiTiet entity = new HoaDonChiTiet();
+                entity.setMaHDCT(rs.getInt("MaHDCT"));
+                entity.setMaHD(rs.getString("MaHD"));
+                entity.setMaMon(rs.getString("MaMon"));
+                entity.setSoLuong(rs.getInt("SoLuong"));
+                entity.setThanhTien(rs.getDouble("ThanhTien"));
+                entity.setGhiChu(rs.getString("GhiChu"));
+                list.add(entity);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+//            e.printStackTrace();
+        }
     }
-
-    
 }
--- src/DAO/HoaDonDAO.java
+++ src/DAO/HoaDonDAO.java
@@ -1,48 +1,86 @@
-
 package DAO;
 
 
 /*
 * Author : Triệu Phan Thiên Bảo - PS22325
-*/
+ */
 import EntityClass.HoaDon;
+import Utils.JdbcHelper;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+public class HoaDonDAO extends QuanLiCFDAO<HoaDon, String> {
 
-public class HoaDonDAO extends QuanLiCFDAO<HoaDon, String>  {
+    String Insert_SQL = "insert into HOADON (MaHD,MaNV,MaKH,NgayLapHD,TongTienHD,GhiChu) values (?,?,?,?,?,?)";
+    String Update_SQL = "update HOADON set MaKH = ?, NgayLapHD = ?, TongTienHD = ?, GhiChu = ? where MaHD = ? ";
+    String Delete_SQL = "delete from HOADON where MaHD = ?";
+    String SelectAll_SQL = "select * from HOADON";
+    String SelectByID_SQL = "select * from HOADON where MaHD = ?";
 
-    
     @Override
     public void insert(HoaDon entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Insert_SQL,
+                entity.getMaHD(),
+                entity.getMaNV(),
+                entity.getMaKH(),
+                entity.getNgayLapHD(),
+                entity.getTongTienHD(),
+                entity.getGhiChu());
     }
 
     @Override
     public void update(HoaDon entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Update_SQL,
+                entity.getMaHD(),
+                entity.getMaNV(),
+                entity.getMaKH(),
+                entity.getNgayLapHD(),
+                entity.getTongTienHD(),
+                entity.getGhiChu());
     }
 
     @Override
     public void delete(String key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JdbcHelper.update(Delete_SQL, key);
     }
 
     @Override
     public List<HoaDon> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return selectBySQL(SelectAll_SQL);
     }
 
     @Override
     public HoaDon selectById(String key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<HoaDon> list = selectBySQL(SelectByID_SQL, key);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
 
     @Override
     protected List<HoaDon> selectBySQL(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<HoaDon> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                HoaDon entity = new HoaDon();
+                entity.setMaHD(rs.getString("MaHD"));
+                entity.setMaNV(rs.getString("MaNV"));
+                entity.setMaKH(rs.getInt("MaKH"));
+                entity.setNgayLapHD(rs.getDate("NgayLapHD"));
+                entity.setTongTienHD(rs.getDouble("TongTienHD"));
+                entity.setGhiChu(rs.getString("GhiChu"));
+                list.add(entity);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+//            e.printStackTrace();
+        }
     }
 
-    
 }
